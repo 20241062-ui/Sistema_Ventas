38a7d927fb9a59094229a1bff4c24324b143509d
PerezDev <20241020@uthh.edu.mx>
2026-03-08 00:09:32 -0600

    Coreccion de Login

--- Backend/src/config/BD.js
+++ Backend/src/config/BD.js
@@ -1,19 +1,19 @@
-import { createPool } from 'mysql2/promise'
+import {createPool} from 'mysql2/promise'
 import dotenv from 'dotenv'
 
-// Configuramos el acceso a nuestras variables de entorno 
+//configuramos el acceso a nuestras variables de entorno 
 dotenv.config()
 
-// Creamos el pool de conexión a la base de datos
-const pool = createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD, // Corregido: llevaba D al final
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 3306,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+//creamos el pool de coneccion a la base de datos
+const pool =createPool({
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_NAME,
+    port:process.env.DB_PORT||3306,
+    waitForConnections:true,
+    connectionLimit:10,
+    queueLimit:0
 })
 
 export default pool
