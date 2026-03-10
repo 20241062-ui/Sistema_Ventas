58a422bf5cf1d2695dafd8167735401bf587cad5
20241062-ui <20241062@uthh.edu.mx>
2026-03-09 21:54:44 -0600

    correccion de rutas

--- Backend/src/routes/productoRoutes.js
+++ Backend/src/routes/productoRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getHomeProductos } from '../controllers/productoController.js';
+// IMPORTANTE: Asegúrate de incluir AMBAS funciones en el import
+import { getHomeProductos, getProductoDetalle } from '../controllers/productoController.js';
 
 const router = express.Router();
 
 router.get('/home', getHomeProductos);
-router.get('/detalle/:id', getProductoDetalle);
+router.get('/detalle/:id', getProductoDetalle); // Línea 7 que mencionaba el error
 
 export default router;
