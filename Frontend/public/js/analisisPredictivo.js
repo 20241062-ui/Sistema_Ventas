0d4782ee03526c80cc430b0b03905f339f41f257
20241062-ui <20241062@uthh.edu.mx>
2026-04-18 15:19:42 -0600

    si

--- Frontend/public/js/analisisPredictivo.js
+++ Frontend/public/js/analisisPredictivo.js
@@ -85,7 +85,7 @@ async function ejecutarPrediccion(noSerie, nombreProducto) {
                         <b style="color: #2c3e50;">Parámetros del Modelo de Decaimiento:</b><br>
                         • Stock Inicial estimado \\( S_0 \\): <b style="color: #2c3e50;">${data.stockInicial} unidades</b><br>
                         • Tasa de decaimiento \\( k \\): <b style="color: #2c3e50;">${data.k}</b><br>
-                        • Tiempo: ${data.tiempo}
+                        • Tiempo: ${data.tiempo}</b><br>
                         • Variación semanal: <b style="color: #2c3e50;">${(Math.abs(data.k) * 100).toFixed(2)}%</b> del inventario disponible.<br>
                         <p style="margin-top: 10px; font-style: italic; color: #7f8c8d;">
                             <strong>Sugerencia:</strong> ${sugerenciaAdmin}
@@ -94,7 +94,6 @@ async function ejecutarPrediccion(noSerie, nombreProducto) {
                 </div>
             `;
 
-            // Forzar a MathJax a procesar el nuevo HTML inyectado
             if (window.MathJax && window.MathJax.typesetPromise) {
                 window.MathJax.typesetPromise([tablaDiv]).catch((err) => console.log("Error MathJax:", err));
             }
